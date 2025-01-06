afb23dd79dee4602ff6633d091f6dcd72f66f1c7
Dylan Lopes <dylananlopes@gmail.com>
2025-01-06 09:11:49 +0000

    created navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import { Button, Link, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, Navbar as NavbarUi } from "@nextui-org/react"
+import { Image } from "@nextui-org/react";
+import React from "react";
+
+const navItems = [
+  { title: "Example", link: "/example" },
+  { title: "About", link: "/about"}
+]
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  return (
+		<NavbarUi
+			isBordered
+			className="bg-white"
+			maxWidth="2xl"
+			isMenuOpen={isMenuOpen}
+			onMenuOpenChange={setIsMenuOpen}
+		>
+			<NavbarContent className="sm:hidden" justify="start">
+				<NavbarMenuToggle
+					aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+					className="text-black"
+				/>
+			</NavbarContent>
+
+			<NavbarContent className="sm:hidden pr-3" justify="center">
+				<NavbarBrand>
+					<Image
+						className="rounded-none"
+						src="/WeatherLogo.svg"
+						alt="Vercel logomark"
+						width={30}
+						height={30}
+					/>
+					<p className="text-black font-bold text-xl ml-2">Weather App</p>
+				</NavbarBrand>
+			</NavbarContent>
+
+			<NavbarContent className="hidden sm:flex" justify="start">
+				<NavbarBrand>
+					<Image
+						className="rounded-none dark:invert"
+						src="/WeatherLogo.svg"
+						alt="Weather App logomark"
+						width={30}
+						height={30}
+					/>
+					<p className="text-black font-bold text-xl ml-2">Weather App</p>
+				</NavbarBrand>
+			</NavbarContent>
+
+			{/* Main Menu */}
+			<NavbarContent className="hidden sm:flex gap-12" justify="center">
+				{navItems.map((item) => (
+					<NavbarItem key={item.title}>
+						<Link href={item.link} className="text-lg text-black font-medium">
+							{item.title}
+						</Link>
+					</NavbarItem>
+				))}
+			</NavbarContent>
+
+			{/* Collapsable Menu */}
+			<NavbarMenu>
+				{navItems.map((item) => (
+					<NavbarItem key={item.title}>
+						<Link href={item.link} className="text-black">
+							{item.title}
+						</Link>
+					</NavbarItem>
+				))}
+			</NavbarMenu>
+
+			<NavbarContent justify="end">
+				<NavbarItem>
+					<Button
+						as={Link}
+						className="bg-indigo-500 text-white font-bold"
+						href="#"
+						variant="flat"
+					>
+						Sign Up
+					</Button>
+				</NavbarItem>
+			</NavbarContent>
+		</NavbarUi>
+	);
+}
